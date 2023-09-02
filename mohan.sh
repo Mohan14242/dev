fa25ce5f02a24f23ad106ad0fd41d3bdd4430772
vijaymichiri <vijay165829@gmail.com>
2023-09-02 17:51:34 +0530

    this is the for my present situation

--- mohan.sh
+++ mohan.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-echo "hi i am learning the devops with aws"
+echo "first i learn sap sd but due to trainer selfishness i am not able to complete it "
+echo "now i am learning the devops with aws by siva sir so pelase bless me"
