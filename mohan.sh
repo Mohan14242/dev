8f61924a07aff75bab240d415dd443ba323de036
vijaymichiri <vijay165829@gmail.com>
2023-09-02 18:15:53 +0530

    this is for mohan

--- mohan.sh
+++ mohan.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-echo "first i learn sap sd but due to trainer selfishness i am not able to complete it "
-echo "now i am learning the devops with aws by siva sir so pelase bless me"
+name="mohan"
+echo "hi my name is echo i am the servant of $name"
