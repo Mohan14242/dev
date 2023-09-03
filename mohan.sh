258dc4807885b14a1bb210c1b1146e786d9e3a83
vijaymichiri <vijay165829@gmail.com>
2023-09-03 20:20:13 +0530

    this is your message

--- mohan.sh
+++ mohan.sh
@@ -4,5 +4,6 @@ userid=$(id -u)
 if [ $userid -ne 0 ]; then 
     echo "please run this command under root user"
 else
-    echo "you are the root user"
-yum install mysql -y
+    echo "you are the root user"    
+    yum install mysql -y
+fi
