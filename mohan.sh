6f0e3ad76c1e35a4ef1f345c03c3cdecce6fd488
vijaymichiri <vijay165829@gmail.com>
2023-09-03 17:12:17 +0530

    Your commit message here

--- mohan.sh
+++ mohan.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-IFS=":"
-line="mohan:chiru:btect"
-
-echo "my name is ${line[0]}"
-echo "my friend name is ${line[1]}"
-echo "we both are studied at ${line[2]}"
+IFS=","
+while read -r name age; do 
+    echo "name:$name ,age:$age"
+done < mohan.csv
