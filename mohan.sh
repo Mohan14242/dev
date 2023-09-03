4cf6a41b73db8ff14af29275d10d89a074e855d1
vijaymichiri <vijay165829@gmail.com>
2023-09-03 20:46:47 +0530

    this is your message

--- mohan.sh
+++ mohan.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-userid=$(id -u)
-if [ $userid -ne 0 ]; then 
-    echo "please run this command under root user"
-    exit 1
-else
-    echo "you are the root user"    
-    yum install mysql -y
-fi
+
+
+validate(){
+    if [ $1 -ne 0 ]; then 
+        echo "installation isgfailure"
+    else
+        ecco "processeing the installation"
+        yum remove mysql -y
+        yum install mysql -y
+    fi
+}
