5b8b85facf70a3df24fa782807bd8bda922dff47
vijaymichiri <vijay165829@gmail.com>
2023-09-03 14:30:17 +0530

    Your commit message here

--- mohan.sh
+++ mohan.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
 echo "this is the mohan"
-name1=$1
-name2=$2
-echo "this is the first name:$name1"
-echo "this is the last name :$name2"
-
+echo "please enter youruser name "
+read username
+echo "user name entered is $username"
