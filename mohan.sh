f63733170a0520ccdabd0620f81716c2840fa170
vijaymichiri <vijay165829@gmail.com>
2023-09-03 16:20:24 +0530

    Your commit message here

--- mohan.sh
+++ mohan.sh
@@ -2,18 +2,4 @@
 
 name1="$1"
 name2="$2"
-echo "the number of arguments passed is $#"
-
-if [ $# -lt 2 ]; then 
-    echo "Sorry, give at least 2 arguments."
-else
-    echo "Right"
-fi
-
-echo "the last argument index is $(($# - 1))"
-
-my_function() {
-    echo "Number of arguments in the function: $#"
-}
-
-my_function "$@"
+echo "the command argument sre $@"
