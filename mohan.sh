3b2e56b68b759289818e1988a2bdc1747c4c0221
vijaymichiri <vijay165829@gmail.com>
2023-09-03 21:10:27 +0530

    this is your message

--- mohan.sh
+++ mohan.sh
@@ -2,7 +2,9 @@
 
 
 
-userid=$(id -u)
+userid=$(date +%f)
+sname=$0
+logfile=/tmp/sname-$userid.log
 validate(){
     if [ $userid -ne 0 ]; then
         echo "please make sure you are root user"
@@ -12,11 +14,11 @@ validate(){
     fi
 }
 
-yum remove mysql -y
+yum remove mysql -y &>>$logfile
 
 validate $? "my sql is installed and preparing for postfix"
 
-yum remove postfix -y
+yum remove postfix -y &>>$logfile
 
 
 
