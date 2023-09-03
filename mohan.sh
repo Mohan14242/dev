5dcbf27a352c3dba216bf961099295c20fa4064f
vijaymichiri <vijay165829@gmail.com>
2023-09-03 15:50:14 +0530

    Your commit message here

--- mohan.sh
+++ mohan.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 
-readonly read="this cannot change"
-echo "this value cant chaneg:$read"
-read="this can change"
-echo "this value can chnage:$read"
+name1=$1
+name2=$2
+echo "the name of the script is $0"
+echo "this is for printing the script path $(readlink -f $0)"
+if [ "$0" ="mohan.sh"]; then 
+    echo "this is my scirpt"
+else
+    echo "this not my script"
+name=$1
+echo "this is the named argument$name"
+echo "Error in $0: Something went wrong." >&2
+scrip_name="$0"
+echo "currrent running script is $scrip_name"
+echo "the directory of the script is $(dirname "$0")"
+
+
