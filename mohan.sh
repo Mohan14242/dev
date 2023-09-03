872ccaa4c69480bc62ed810c9b20b6fd4828f79b
vijaymichiri <vijay165829@gmail.com>
2023-09-03 14:33:16 +0530

    Your commit message here

--- mohan.sh
+++ mohan.sh
@@ -2,5 +2,8 @@
 
 echo "this is the mohan"
 echo "please enter youruser name "
-read username
+read -s username
 echo "user name entered is $username"
+echo "please enter the password"
+read -s password
+echo "the password is $password"
