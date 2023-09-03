248719c39a59e1f73929b95942e5ca104271afcc
vijaymichiri <vijay165829@gmail.com>
2023-09-03 13:00:38 +0530

    Your commit message here

--- mohan.sh
+++ mohan.sh
@@ -3,3 +3,6 @@
 my_string=("mohan","chiru","vijay")
 echo "my name friends name is ${my_string[0]}"
 echo "Script name: $0"
+echo "Number of arguments: $#"
+echo "All arguments: $@"
+echo "Exit status of the last command: $?"
