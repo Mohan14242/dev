031ba833719298f11d3f7bc8d8dfd4e4c4340a5d
vijaymichiri <vijay165829@gmail.com>
2023-09-04 10:22:08 +0530

    this is the mohan

--- mohan.sh
+++ mohan.sh
@@ -4,11 +4,10 @@
 user=$(id -u)
 for i in $@
 do 
-    if [ $user -eq 0 ]
-        if ! yum list installed $i &>/dev/null; then
-            echo "$i is not installed installing"
-            sudo yum install mysql -y
-            echo "$i has been installed"
+    if ! yum list installed $i &>/dev/null; then
+        echo "$i is not installed installing"
+        sudo yum install mysql -y
+        echo "$i has been installed"
     else
         echo "package already installed"
     fi
