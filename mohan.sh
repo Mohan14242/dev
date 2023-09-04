d2bbac556b14537530eff8ba931f2172be6f2c5e
vijaymichiri <vijay165829@gmail.com>
2023-09-04 10:20:19 +0530

    this is the mohan

--- mohan.sh
+++ mohan.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
 
-
-r="\e[31m"
-g="\e[32m"
-n="\e[0m"
-echo -e " this is the $r color"
-echo -e "$g  this is the color $n"
-echo " this is the monan"
-
-
+user=$(id -u)
+for i in $@
+do 
+    if [ $user -eq 0 ]
+        if ! yum list installed $i &>/dev/null; then
+            echo "$i is not installed installing"
+            sudo yum install mysql -y
+            echo "$i has been installed"
+    else
+        echo "package already installed"
+    fi
+done
 
 
 
