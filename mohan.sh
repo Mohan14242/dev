d5558ad710e3c6a51d074538eb9fad4f3ebb1f2d
vijaymichiri <vijay165829@gmail.com>
2023-09-04 09:55:51 +0530

    this is the mohan

--- mohan.sh
+++ mohan.sh
@@ -5,8 +5,8 @@
 r="\e[31m"
 g="\e[32m"
 n="\e[0m"
-echo "$r this is the color"
-echo "$g  this is the color"
+echo -e "$r this is the color"
+echo -e "$g  this is the color"
 echo " this is the monan"
 
 
